40ddb8a2ba109d95cbcff3ef76be819d04b22ce9
Thales Davi de Souza <thalesdavisoul@gmail.com>
2025-07-02 20:03:01 -0300

    [TP3] Lendo a netrada de um arquivo de entrada

--- TP3/src/main.cpp
+++ TP3/src/main.cpp
@@ -2,14 +2,27 @@
 #include "event.hpp"
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    // Verifica se o nome do arquivo foi passado como argumento
+    if (argc != 2) {
+        cerr << "Uso: " << argv[0] << " <arquivo_de_entrada>" << endl;
+        return 1;
+    }
+
     LogisticsSystem system;
     string line;
-    
-    while (getline(cin, line)) {
+    ifstream inputFile(argv[1]); // Abre o arquivo de entrada
+
+    if (!inputFile.is_open()) {
+        cerr << "Erro ao abrir o arquivo: " << argv[1] << endl;
+        return 1;
+    }
+
+    while (getline(inputFile, line)) {
         if (line.empty()) continue;
         
         if (line.find("EV") != string::npos) {
@@ -26,6 +39,7 @@ int main() {
             system.processQuery(line);
         }
     }
-    
+
+    inputFile.close(); // Fecha o arquivo
     return 0;
 }
